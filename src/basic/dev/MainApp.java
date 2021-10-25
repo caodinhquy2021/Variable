d5180d6387f18f0ee0bd11efe63862e76508cd8b
quy <caodinhquyaaa1@gmail.com>
2021-10-25 15:49:20 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,21 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String firstName = "Cao Dinh";
+    String lastName = "Quy";
+    int old = 15;
+    float markMath = 9f;
+    boolean passed = true;
+    
+    System.out.println("Họ và tên");
+    System.out.println(firstName + " " +lastName);
+    System.out.println("Mark Math: " + markMath);
+    System.out.println("Passed: " + passed);
+    
+    final float PI = 3.14f;
+	}
+
+}
